47029fbe1a129346f38d31f17ae3ce3e99c846a8
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2021-02-04 23:11:43 +0200

    Changed structure to diff projects

---
+++ services/dataservice/app/api/service.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+import dataservice.app.api.dataprocessor as processor
+
+dataservice = APIRouter()
+
+@dataservice.get('/ping')
+def health_check():
+    return 'API is running'
+
+@dataservice.post('/execute')
+def prepare_data():
+
+    norm_train_X, norm_test_X, norm_val_X, train_Y, test_Y, val_Y = processor.prepare_datasets()
+
+    return True
