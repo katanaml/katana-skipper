11bce4c48f8fd61669e47fd4673b54af7bf65128
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2021-05-10 14:39:28 +0300

    RabbitMQ support

--- README.md
+++ README.md
@@ -5,6 +5,17 @@ This is a simple and flexible ML workflow engine. It helps to orchestrate events
 
 Katana ML, Red Samurai Consulting, Andrej Baranovskij
 
+## Instructions
+
+### Start/Stop
+
+docker-compose up --build -d
+docker-compose down
+
+### URLs
+
+* RabbitMQ: http://localhost:15672/ (skipper/welcome1)
+
 ## License
 
 Licensed under the Apache License, Version 2.0. Copyright 2020-2021 Katana ML. [Copy of the license](https://github.com/katanaml/katana-pipeline/blob/master/LICENSE).
---
+++ rabbitmq/init.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+(
+count=0;
+# Execute list_users until service is up and running
+until timeout 5 rabbitmqctl list_users >/dev/null 2>/dev/null || (( count++ >= 60 )); do sleep 1; done;
+
+# Enable plugins
+rabbitmq-plugins enable rabbitmq_management
+
+# Register user
+if rabbitmqctl list_users | grep $RABBITMQ_USER > /dev/null
+then
+  echo "User '$RABBITMQ_USER' already exist, skipping user creation"
+else
+  echo "Creating user '$RABBITMQ_USER'..."
+  rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD
+  rabbitmqctl set_user_tags $RABBITMQ_USER administrator
+  rabbitmqctl set_permissions -p / $RABBITMQ_USER ".*" ".*" ".*"
+  echo "User '$RABBITMQ_USER' creation completed"
+fi
+) &
+
+# Call original entrypoint
+exec docker-entrypoint.sh rabbitmq-server $@
