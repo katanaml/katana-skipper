50802db2f85abe1082e79930f9d8337df172c038
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2021-05-14 00:03:09 +0300

    RabbitMQ integration into API

--- engine/api/router.py
+++ engine/api/router.py
@@ -3,6 +3,7 @@ from .models import WorkflowTask, WorkflowTaskResult, WorkflowTaskData
 from .tasks import process_workflow
 from celery.result import AsyncResult
 from fastapi.responses import JSONResponse
+import json
 
 router_tasks = APIRouter()
 
@@ -14,9 +15,15 @@ def touch():
 
 @router_tasks.post('/start', response_model=WorkflowTask, status_code=202)
 async def start_workflow_task(workflow_task_data: WorkflowTaskData):
-    task_id = process_workflow.delay(workflow_task_data.task_type,
-                                     workflow_task_data.payload,
-                                     workflow_task_data.description)
+    payload = {
+        'task_type': workflow_task_data.task_type,
+        'payload': workflow_task_data.payload,
+        'description': workflow_task_data.description
+    }
+    payload_json = json.dumps(payload, indent=4)
+
+    task_id = process_workflow.delay(workflow_task_data.task_type, payload_json)
+
     return {'task_id': str(task_id),
             'task_status': 'Processing'}
 
--- engine/api/tasks.py
+++ engine/api/tasks.py
@@ -1,4 +1,5 @@
 from .worker import app
+from .backend.event_producer import EventProducer
 from celery.utils.log import get_task_logger
 
 # Create logger - enable to display messages on task logger
@@ -6,8 +7,9 @@ celery_log = get_task_logger(__name__)
 
 
 @app.task(name='api.process_workflow')
-def process_workflow(task_type, payload, description):
+def process_workflow(task_type, data):
+    event_producer = EventProducer()
+    response = event_producer.call(data)
 
-
-    celery_log.info(task_type + " task completed!")
-    return 'OK'
+    celery_log.info(task_type + " task completed")
+    return response
