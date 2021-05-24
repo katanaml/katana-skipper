4cfea6675adbe8da1a16525ffecb5ab9547a3a8e
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2021-05-24 14:54:44 +0300

    Serving service

---
+++ services/servingservice/app/queue/event_receiver.py
@@ -0,0 +1,32 @@
+import pika
+from app.serving_service import ServingService
+
+
+class EventReceiver(object):
+    def __init__(self):
+        credentials = pika.PlainCredentials('skipper', 'welcome1')
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost',
+                                                                       port=5672,
+                                                                       credentials=credentials))
+
+        channel = connection.channel()
+        channel.queue_declare(queue='skipper_serving')
+
+        channel.basic_qos(prefetch_count=1)
+        channel.basic_consume(queue='skipper_serving', on_message_callback=self.on_request)
+
+        print("Awaiting requests for [x] serving service [x]")
+        channel.start_consuming()
+
+    def on_request(self, ch, method, props, body):
+        pass
+        # training_service = TrainingService()
+        # response, task_type = training_service.call(body)
+
+        # ch.basic_publish(exchange='',
+        #                  routing_key=props.reply_to,
+        #                  properties=pika.BasicProperties(correlation_id=props.correlation_id),
+        #                  body=response)
+        # ch.basic_ack(delivery_tag=method.delivery_tag)
+
+        # print('Processed request:', task_type)
---
+++ services/servingservice/app/serving_service.py
@@ -0,0 +1,9 @@
+
+
+
+class ServingService(object):
+    def __init__(self):
+        pass
+
+    def call(self, data):
+        pass
