51997b32643575c0543f04df22f8ef5600e69c13
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2021-07-18 15:02:29 +0300

    Nginx support and improvements

--- api/endpoint.py
+++ api/endpoint.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.router import router_tasks
 
-app = FastAPI()
+app = FastAPI(openapi_url="/api/v1/skipper/tasks/openapi.json", docs_url="/api/v1/skipper/tasks/docs")
 
 app.add_middleware(
     CORSMiddleware,
