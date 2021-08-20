9e9cfe52b00d4e61c6b7490f029c8915b8dd24c1
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2021-08-20 16:34:07 +0300

    Fix for RabbitMQ dockerfile

--- rabbitmq/init.sh
+++ rabbitmq/init.sh
@@ -6,9 +6,6 @@ count=0;
 # Execute list_users until service is up and running
 until timeout 5 rabbitmqctl list_users >/dev/null 2>/dev/null || (( count++ >= 60 )); do sleep 1; done;
 
-# Enable plugins
-rabbitmq-plugins enable rabbitmq_management
-
 # Register user
 if rabbitmqctl list_users | grep $RABBITMQ_USER > /dev/null
 then
