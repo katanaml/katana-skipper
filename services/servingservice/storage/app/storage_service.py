d3069a6a7ab94457a0ad89a37b8ea34bff06eab4
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2021-10-16 23:37:34 +0300

    Serving service update to support Kubernetes multi node

--- services/servingservice/storage/app/storage_service.py
+++ services/servingservice/storage/app/storage_service.py
@@ -17,14 +17,17 @@ class StorageService(object):
         model_decoded = base64.b64decode(data_json['model'])
         stats_decoded = base64.b64decode(data_json['stats'])
 
-        with open(os.getenv('MODELS_FOLDER', '../../models/serving/') + archive_name, 'wb') as model_file_decoded:
-            model_file_decoded.write(model_decoded)
+        try:
+            with open(os.getenv('MODELS_FOLDER', '../../models/serving/') + archive_name, 'wb') as model_file_decoded:
+                model_file_decoded.write(model_decoded)
 
-        shutil.unpack_archive(os.getenv('MODELS_FOLDER', '../../models/serving/') + archive_name,
-                              os.getenv('MODELS_FOLDER', '../../models/serving/') + model_name,
-                              format='zip')
+            shutil.unpack_archive(os.getenv('MODELS_FOLDER', '../../models/serving/') + archive_name,
+                                  os.getenv('MODELS_FOLDER', '../../models/serving/') + model_name,
+                                  format='zip')
 
-        with open(os.getenv('MODELS_FOLDER', '../../models/serving/') + stats_name, 'wb') as stats_file_decoded:
-            stats_file_decoded.write(stats_decoded)
+            with open(os.getenv('MODELS_FOLDER', '../../models/serving/') + stats_name, 'wb') as stats_file_decoded:
+                stats_file_decoded.write(stats_decoded)
+        except:
+            print('Model file saving ignored, sharing same node')
 
         return 'OK', 'storage'
