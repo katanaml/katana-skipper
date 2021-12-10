03ebb61356f47758212f19ca4b862e9dc3e3753a
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2021-12-10 22:51:51 +0200

    Configurable FastAPI endpoints

--- api/endpoint.py
+++ api/endpoint.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.routers import skipper, boston, mobilenet
+import os
 
 app = FastAPI(openapi_url="/api/v1/skipper/tasks/openapi.json",
               docs_url="/api/v1/skipper/tasks/docs")
@@ -13,6 +14,11 @@ app.add_middleware(
     allow_credentials=True,
 )
 
+boston_enabled = os.getenv('BOSTON_ENABLED', 'y')
+mobilenet_enabled = os.getenv('MOBILENET_ENABLED', 'y')
+
 app.include_router(skipper.router, prefix='/api/v1/skipper/tasks')
-app.include_router(boston.router, prefix='/api/v1/skipper/tasks')
-app.include_router(mobilenet.router, prefix='/api/v1/skipper/tasks')
+if boston_enabled == 'y':
+    app.include_router(boston.router, prefix='/api/v1/skipper/tasks')
+if mobilenet_enabled == 'y':
+    app.include_router(mobilenet.router, prefix='/api/v1/skipper/tasks')
